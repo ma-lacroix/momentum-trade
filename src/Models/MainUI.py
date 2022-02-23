c90f0101ab523df1d4ecff5c0eb883264fb62704
ma-lacroix <lacroix.marcantoine@gmail.com>
2022-02-23 22:08:47 +0100

    UI mods

--- src/Models/MainUI.py
+++ src/Models/MainUI.py
@@ -1,49 +1,67 @@
 import tkinter as tk
-from tkinter import Button, Label, END, Entry, ttk
+from tkinter import ttk
 
 
 class MainUI(tk.Tk):
-
-    """Main window container"""
-
-    def __init__(self , name) -> tk.Tk:
+    # TODO: clean this up
+    def __init__(self, name):
         super().__init__()
-        self.style = Style(self)
-        self.style.configure('Button', font=('Arial', 12))
-        self.style.configure('Label', font=('Arial', 12))
-        self.geometry("400x300+10+10")
-        self.lbl1 = Label(text='First number')
-        self.lbl2 = Label(text='Second number')
-        self.lbl3 = Label(text='Result')
-        self.title(name)
-        self.t1 = Entry(bd=3)
-        self.t2 = Entry()
-        self.t3 = Entry()
-        self.btn1 = Button(text='Add')
-        self.btn2 = Button(text='Subtract')
-        self.lbl1.place(x=10, y=50)
-        self.t1.place(x=100, y=50)
-        self.lbl2.place(x=10, y=100)
-        self.t2.place(x=100, y=100)
-        self.b1 = Button(text='Add', command=self.add)
-        self.b2 = Button(text='Subtract')
-        self.b2.bind('<Button-1>', self.sub)
-        self.b1.place(x=10, y=150)
-        self.b2.place(x=100, y=150)
-        self.lbl3.place(x=100, y=200)
-        self.t3.place(x=200, y=200)
 
-    def add(self):
-        self.t3.delete(0, 'end')
-        num1=int(self.t1.get())
-        num2=int(self.t2.get())
-        result=num1+num2
-        self.t3.insert(END, str(result))
+        self.geometry('300x300')
+        self.resizable(0, 0)
+        self.title = name
 
-    def sub(self, event):
-        self.t3.delete(0, 'end')
-        num1=int(self.t1.get())
-        num2=int(self.t2.get())
-        result=num1-num2
-        self.t3.insert(END, str(result))
+        # UI options
+        paddings = {'padx': 5, 'pady': 5}
+        entry_font = {'font': ('Helvetica', 14)}
 
+        # configure the grid
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+
+        self.roc_timeframe = tk.StringVar()
+        self.total_number_stocks = tk.StringVar()
+        self.max_security_price = tk.StringVar()
+        self.total_budget = tk.StringVar()
+
+        heading = ttk.Label(self, text='Generate Report', style='Heading.TLabel')
+        heading.grid(column=0, row=0, columnspan=2, pady=5, sticky=tk.N)
+
+        budget_label = ttk.Label(self, text="Total Budget:")
+        budget_label.grid(column=0, row=1, sticky=tk.W, **paddings)
+
+        budget_entry = ttk.Entry(self, textvariable=self.total_budget, **entry_font)
+        budget_entry.grid(column=1, row=1, sticky=tk.E, **paddings)
+
+        roc_label = ttk.Label(self, text="Total Budget:")
+        roc_label.grid(column=0, row=2, sticky=tk.W, **paddings)
+
+        roc_entry = ttk.Entry(self, textvariable=self.roc_timeframe, **entry_font)
+        roc_entry.grid(column=1, row=2, sticky=tk.E, **paddings)
+        #
+        stocks_label = ttk.Label(self, text="Total Budget:")
+        stocks_label.grid(column=0, row=3, sticky=tk.W, **paddings)
+
+        stocks_entry = ttk.Entry(self, textvariable=self.total_number_stocks, **entry_font)
+        stocks_entry.grid(column=1, row=3, sticky=tk.E, **paddings)
+        #
+        price_label = ttk.Label(self, text="Total Budget:")
+        price_label.grid(column=0, row=4, sticky=tk.W, **paddings)
+
+        price_entry = ttk.Entry(self, textvariable=self.max_security_price, **entry_font)
+        price_entry.grid(column=1, row=4, sticky=tk.E, **paddings)
+        # login button
+        login_button = ttk.Button(self, text="Login", command=self.print_something)
+        login_button.grid(column=1, row=5, sticky=tk.E, **paddings)
+
+        # configure style
+        self.style = ttk.Style(self)
+        self.style.configure('TLabel', font=('Helvetica', 14))
+        self.style.configure('TButton', font=('Helvetica', 14))
+
+        # heading style
+        self.style.configure('Heading.TLabel', font=('Helvetica', 16))
+
+    def print_something(self):
+        print(f"{self.title} - {self.roc_timeframe} - {self.total_number_stocks} - {self.total_budget} \
+                {self.max_security_price}")
